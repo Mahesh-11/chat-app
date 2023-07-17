775b9d494043211293d7071e9ac7cc1681b2dbdd
mahesh-2 <maheshpeddapuramyadav929@gmail.com>
2023-07-17 23:06:29 +0530

    test_1
    checking for localhost and ip change

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'http://64.227.157.27:3000',
         methods: ['GET', 'POST'],
     },
 });
